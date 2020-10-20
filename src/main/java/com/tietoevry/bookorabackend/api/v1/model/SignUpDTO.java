c8b9c2d0c82fb26d715e139bd35509bd1bd03f27
Ali <ali2016zedan@gmail.com>
2020-10-20 11:34:50 +0200

    Added password validation .

--- src/main/java/com/tietoevry/bookorabackend/api/v1/model/SignUpDTO.java
+++ src/main/java/com/tietoevry/bookorabackend/api/v1/model/SignUpDTO.java
@@ -1,5 +1,6 @@
 package com.tietoevry.bookorabackend.api.v1.model;
 
+import com.tietoevry.bookorabackend.validation.ValidPassword;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -25,6 +26,7 @@ public class SignUpDTO {
     private String email;
 
     @NotBlank @Size(min = 6, max = 40)
+    @ValidPassword
     private String password;
 
     private Set<String> roles;
