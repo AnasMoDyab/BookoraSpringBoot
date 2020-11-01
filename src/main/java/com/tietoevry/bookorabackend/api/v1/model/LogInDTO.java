224b6ebe5031b1d52a5e72293bcc573bb75242f9
ali123321123 <ali2016zedan@gmail.com>
2020-11-01 13:04:00 +0100

    Fixed login for activited account #26

--- src/main/java/com/tietoevry/bookorabackend/api/v1/model/LogInDTO.java
+++ src/main/java/com/tietoevry/bookorabackend/api/v1/model/LogInDTO.java
@@ -1,9 +1,11 @@
 package com.tietoevry.bookorabackend.api.v1.model;
 
+import com.tietoevry.bookorabackend.validation.ValidPassword;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
 @Data
@@ -12,9 +14,11 @@ import javax.validation.constraints.NotBlank;
 public class LogInDTO {
 
     @NotBlank
+    @Email
     private String email;
 
     @NotBlank
+    @ValidPassword
     private String password;
 
 }
