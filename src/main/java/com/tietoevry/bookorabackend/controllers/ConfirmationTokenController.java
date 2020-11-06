b9a706efd4da0e9640f7aebce235add3f56610a2
ali123321123 <ali2016zedan@gmail.com>
2020-11-06 13:32:47 +0100

    Fixed redirection

--- src/main/java/com/tietoevry/bookorabackend/controllers/ConfirmationTokenController.java
+++ src/main/java/com/tietoevry/bookorabackend/controllers/ConfirmationTokenController.java
@@ -3,10 +3,11 @@ package com.tietoevry.bookorabackend.controllers;
 import com.tietoevry.bookorabackend.services.ConfirmationTokenService;
 import com.tietoevry.bookorabackend.services.EmployeeServiceImp;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
-@RestController
+@Controller
 public class ConfirmationTokenController {
 
     private final ConfirmationTokenService confirmationTokenService;
