b9228e03eff705016e5134f62542706fa60f642f
Roy Hung <43883872+arroy114@users.noreply.github.com>
2020-11-20 15:24:33 +0100

    Updated integration test for Zone status

--- src/test/java/com/tietoevry/bookorabackend/integrationTest/ZoneStatusIT.java
+++ src/test/java/com/tietoevry/bookorabackend/integrationTest/ZoneStatusIT.java
@@ -15,11 +15,13 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.*;
+import org.springframework.web.client.ResourceAccessException;
 
 import java.time.LocalDate;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.springframework.http.HttpStatus.OK;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -69,5 +71,23 @@ public class ZoneStatusIT {
         assertThat(response.getBody().get(0).getTotalReservation()).isNotNull();
         assertThat(response.getBody().get(0).getBookedPercentage()).isNotNull();
         assertThat(response.getBody().size()).isNotNull();
+    }
+
+    @DisplayName("Check status of all zone in a floor with invalid JWT")
+    @Test
+    void checkStatusOfAllZoneInAFloorWithInvalidJWT() {
+        //given
+        FloorDateDTO floorDateDTO = new FloorDateDTO(1, LocalDate.now());
+        headers.set("Authorization", "Bearer " + inValidJwt);
+        HttpEntity<FloorDateDTO> request = new HttpEntity<>(floorDateDTO, headers);
+
+        //when
+        assertThatThrownBy(() -> {
+            restTemplate
+                    .exchange("http://localhost:" + port + ZoneController.BASE_URL + "/checkStatusOfAllZoneInAFloor"
+                            , HttpMethod.POST, request,  new ParameterizedTypeReference<List<StatusOfAZoneOnADayDTO>>(){});
+        })
+                //then
+                .isInstanceOf(ResourceAccessException.class);
     }
 }
