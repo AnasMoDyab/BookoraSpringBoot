f27900e7820f4d8a3bc487b308be826a20985735
Roy Hung <43883872+arroy114@users.noreply.github.com>
2020-11-25 11:38:45 +0100

    Wrote JavaDoc to EmployeeController

--- src/main/java/com/tietoevry/bookorabackend/controllers/EmployeeController.java
+++ src/main/java/com/tietoevry/bookorabackend/controllers/EmployeeController.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
+/**
+ * A rest controller that provides API for managing employees.
+ */
 @CrossOrigin(origins = "*", maxAge = 3600)
 @Tag(name = "Employee", description = "Employee API")
 @RestController
@@ -38,6 +41,14 @@ public class EmployeeController {
         return employeeService.getEmployeeById(id);
     }*/
 
+    /**
+     * Provides information about a selected employee.
+     *
+     * @param email A string of email
+     * @return An EmployeeDTO that contains information of the employee including ID, first name, last name, email,
+     * password and role
+     * @throws Exception EmployeeNotFoundException if employee is not found
+     */
     @GetMapping("/email/{email}")
     @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     @ResponseStatus(HttpStatus.OK)
@@ -45,12 +56,29 @@ public class EmployeeController {
         return employeeService.getEmployeeByEmail(email);
     }
 
+    /**
+     * Create a new employee.
+     *
+     * @param signUpDTO A SignUpDTO that contains information about employee, including last name, first name, email,
+     *                  password and set of roles
+     * @return A MessageDTO that contains message string about the creation of a new employee
+     * @throws Exception InvalidDomainException if the email domain is not valid
+     */
     @PostMapping("/signup")
     @ResponseStatus(HttpStatus.CREATED)
     public MessageDTO createNewEmployee(@RequestBody @Valid SignUpDTO signUpDTO) throws Exception {
         return employeeService.createNewEmployee(signUpDTO);
     }
 
+    /**
+     * Signs in a employee.
+     *
+     * @param logInDTO A LoginDTO that contains email and password of the employee
+     * @return A JwtDTO that contains information including token string, token type, token ID, email of the employee
+     * and list of roles of the employee
+     * @throws Exception EmployeeNotFoundException if the employee is not found
+     * @throws Exception EmployeeNotActivatedException if the email account is not activated
+     */
     @PostMapping("/signin")
     @ResponseStatus(HttpStatus.OK)
     //todo Block user after attemping 3 times to login with wrong password(Brute-force attacks)
@@ -58,6 +86,13 @@ public class EmployeeController {
         return employeeService.logIn(logInDTO);
     }
 
+    /**
+     * Updates an employee.
+     *
+     * @param emailDTO A EmailDTO that contains email of the employee and the set of roles
+     * @return A MessageDTO that contains message about the updating
+     * @throws Exception EmployeeNotFoundException if the employee is not found
+     */
     @PostMapping("/updateEmployee")
     @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     @ResponseStatus(HttpStatus.OK)
@@ -72,13 +107,29 @@ public class EmployeeController {
         employeeService.deleteEmployeeDTO(id);
     }*/
 
-    // Receive the address and send an email
+    /**
+     * Requests an activation code to for resetting password.
+     *
+     * <p>The activation code is sent though email.
+     *
+     * @param forgetPasswordDTO A DTO that contains an email string
+     * @return A MessageDTO that contains message about the request
+     * @throws EmployeeNotFoundException if the employee is not found
+     */
     @PostMapping({"/forgot-password"})
     public MessageDTO forgotUserPassword(@Valid @RequestBody ForgetPasswordDTO forgetPasswordDTO) throws EmployeeNotFoundException {
         return employeeService.sendForgetPasswordCode(forgetPasswordDTO);
     }
 
-    //ReActivation Account
+    /**
+     * Requests for reseeding an activation link for an account.
+     *
+     * <p>The activation link is sent though email.
+     *
+     * @param reActiveEmailDTO A DTO that contains an email string
+     * @return A MessageDTO that contains message about the request
+     * @throws EmployeeNotFoundException if the employee is not found
+     */
     @RequestMapping(value = "/reactive-account", method = RequestMethod.POST)
     public MessageDTO resetUserPassword(@RequestBody @Valid ReActiveEmailDTO reActiveEmailDTO) throws EmployeeNotFoundException {
         return employeeService.resendConfirmationToken(reActiveEmailDTO);
