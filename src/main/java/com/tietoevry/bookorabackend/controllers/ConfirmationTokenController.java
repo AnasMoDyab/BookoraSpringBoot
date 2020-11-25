25382a9769cc7858bb72916558f59268bd6f2f6b
Roy Hung <43883872+arroy114@users.noreply.github.com>
2020-11-25 10:15:15 +0100

    Wrote JavaDoc to ConfirmationTokenController

--- src/main/java/com/tietoevry/bookorabackend/controllers/ConfirmationTokenController.java
+++ src/main/java/com/tietoevry/bookorabackend/controllers/ConfirmationTokenController.java
@@ -9,6 +9,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+/**
+ * A controller that provides API for email verification after sign up.
+ *
+ * <p>The controller redirects user to different pages based on the confirmation token that is sent.
+ */
 @CrossOrigin(origins = "*", maxAge = 3600)
 @Controller
 public class ConfirmationTokenController {
@@ -19,6 +24,14 @@ public class ConfirmationTokenController {
         this.confirmationTokenService = confirmationTokenService;
     }
 
+    /**
+     * Verifies confirmation token.
+     *
+     * <p>User will be redirected to different in pages pages based on the confirmation token that is sent.
+     *
+     * @param token A confirmation token string
+     * @return A DTO that contains message about the result of verification
+     */
     @GetMapping("/confirm-account")
     @ResponseStatus(HttpStatus.OK)
     public String messageDTO(@RequestParam("token") String token) {
